3b5feff0e430a13ffb560d2fe234aacd6dfdf83b
CarCode <carcode@me.com>
2013-10-23 13:05:36 +0200

    Update OCPN as of diffs by D.Register 22.10.2013

--- include/chartimg.h
+++ include/chartimg.h
@@ -213,6 +213,8 @@ protected:
       virtual int BSBScanScanline(wxInputStream *pinStream);
       virtual int ReadBSBHdrLine( wxFileInputStream*, char *, int );
       virtual int AnalyzeRefpoints(void);
+      virtual bool AnalyzeSkew(void);
+
       virtual bool SetMinMax(void);
 
       InitReturn PreInit( const wxString& name, ChartInitFlag init_flags, ColorScheme cs );
