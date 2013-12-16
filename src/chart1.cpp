3442b84755afaa21f684fb615754c9c0ab52569e
CarCode <carcode@me.com>
2013-12-16 15:53:16 +0100

    Update OCPN as of diffs by D.Registrer 15.12.2013

--- src/chart1.cpp
+++ src/chart1.cpp
@@ -710,8 +710,8 @@ catch_signals(int signo)
         break;
 
         case SIGTERM:
+        LogMessageOnce(_T("Sigterm received"));
         gFrame->Close();
-        abort();
         break;
 
         default:
