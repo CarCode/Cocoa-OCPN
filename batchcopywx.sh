874b5d8b67c271071896ed44069f9f879850b9d5
CarCode <carcode@me.com>
2019-11-27 14:37:23 +0100

    Update Nov. 2019

---
+++ batchcopywx.sh
@@ -0,0 +1,15 @@
+# 3.1.2 oder 3.1.3 oder 3.1.4 einsetzen
+#
+
+cp /usr/local/lib/libwx_baseu_net-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_baseu_xml-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_baseu-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_osx_cocoau_adv-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_osx_cocoau_aui-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_osx_cocoau_core-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_osx_cocoau_gl-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_osx_cocoau_html-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_osx_cocoau_qa-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_osx_cocoau_webview-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+cp /usr/local/lib/libwx_osx_cocoau_xrc-3.1.3.0.0.dylib ~/devel/otool/Frameworks
+echo "Fertig Kopie an otool/Frameworks"
