cd8dfb424e6b636adf0aeefc0953969de58507c3
CarCode <carcode@me.com>
2021-05-04 09:38:43 +0200

    Update

--- plugins/dashboard_pi/src/dashboard_pi.cpp
+++ plugins/dashboard_pi/src/dashboard_pi.cpp
@@ -463,7 +463,7 @@ bool dashboard_pi::DeInit( void )
 
     return true;
 }
-/*  update_path etc. SignalK nicht übernommen. Siehe auch Änderung "Merge pull request #2010 from Hakans" 2.8.2020 und commit "Correct JSON parsing..." vom 16.01.2021
+/*  update_path etc. SignalK nicht übernommen. Siehe auch Änderung "Merge pull request #2010 from Hakans" 2.8.2020 und commit "Correct JSON parsing..." vom 16.01.2021 und Commit vom 29.04.2021
 double GetJsonDouble(wxJSONValue &value) {
     double d_ret = 0;
     if (value.IsDouble()) {
@@ -963,7 +963,7 @@ void dashboard_pi::SetNMEASentence( wxString &sentence )
                     mPriWDN = 3;
                     if (!std::isnan(m_NMEA0183.Mwd.WindAngleTrue)) { //if WindAngleTrue is available, use it ...
                         SendSentenceToAllInstruments(OCPN_DBP_STC_TWD, m_NMEA0183.Mwd.WindAngleTrue,
-                                                     _T("\u00B0T"));
+                                                     _T("\u00B0"));
                         mWDN_Watchdog = gps_watchdog_timeout_ticks;
                     }
                     else if (!std::isnan(m_NMEA0183.Mwd.WindAngleMagnetic)) { //otherwise try WindAngleMagnetic ...
@@ -1033,13 +1033,15 @@ void dashboard_pi::SetNMEASentence( wxString &sentence )
                                                              m_twaangle, m_twaunit);
 
                                 if (mPriWDN >= 4) {
-                                    //MWV has wind angle relative to the bow. Wind history use angle relative to north.
-                                    //If no TWD with higher priority is present and true heading is available calculate it.
+                                    // m_twaangle_raw has wind angle relative to the bow.
+                                    // Wind history use angle relative to north.
+                                    // If no TWD with higher priority is present and
+                                    // true heading is available calculate it.
                                     if (g_dHDT < 361. && g_dHDT >= 0.0) {
                                         double g_dCalWdir = (m_NMEA0183.Mwv.WindAngle) + g_dHDT;
-                                        if (g_dCalWdir > 360.) { g_dCalWdir = g_dCalWdir - 360; }
-                                        else if (g_dCalWdir < 0.) { g_dCalWdir = 360 - g_dCalWdir; }
-                                        SendSentenceToAllInstruments(OCPN_DBP_STC_TWD, g_dCalWdir, _T("\u00B0T"));
+                                        if (g_dCalWdir > 360.) { g_dCalWdir -= 360; }
+                                        else if (g_dCalWdir < 0.) { g_dCalWdir += 360; }
+                                        SendSentenceToAllInstruments(OCPN_DBP_STC_TWD, g_dCalWdir, _T("\u00B0"));
                                         mPriWDN = 4;
                                         mWDN_Watchdog = gps_watchdog_timeout_ticks;
                                     }
